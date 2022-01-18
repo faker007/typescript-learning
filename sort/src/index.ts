3579e44661474509445c724e1c4016c340f4f1b9
faker007 <rlaqjadud195@naver.com>
2022-01-18 10:31:04 +0900

    develop implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numberCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numberCollection);
+// const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numberCollection);
+// sorter.sort();
+// console.log(numberCollection.data);
+
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numberCollection.data);
+console.log(charactersCollection.data);
